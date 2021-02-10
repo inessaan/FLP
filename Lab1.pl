17f025aec3b71de2eccb1405a10e05a0834c6f8d
Inessa An <72630506+inessaan@users.noreply.github.com>
2021-02-10 13:30:20 +0300

    Update Lab1.pl
    Задание 12. Построила предикат uncle(X,Y) , который проверяет, является ли X дядей
    Y и предикат uncles(X), который выводит всех дядей X.

--- Lab1.pl
+++ Lab1.pl
@@ -93,23 +93,33 @@ parent(roksana,duhovlad).
 
 men:-man(X),write(X),write(","),fail.
 women:- woman(X), write(X), write(","),fail.
+
 children(X,Y):-parent(Y,X).
 allchildren(Y):-children(X,Y),write(X),write(","),fail.
+
 mother(X,Y):-parent(X,Y),woman(X).
 mother(X):-mother(Y,X),write(Y).
+
 father(X,Y):-parent(X,Y),man(X),!.
 father(X):-father(Y,X),write(Y).
+
 brother(X,Y):-parent(Z,X),parent(Z,Y),man(Z),man(X),not(X=Y),!.
 brother_for_all(X,Y):-parent(Z,X),parent(Z,Y),man(Z),man(X),not(X=Y).
 brothers(X):-brother_for_all(Y,X),write(Y),write(","),fail.
+
 sister(X,Y):-parent(Z,X),parent(Z,Y),man(Z),woman(X),not(X=Y),!.
 sister_for_all(X,Y):-parent(Z,X),parent(Z,Y),man(Z),woman(X),not(X=Y).
 sisters(X):-sister_for_all(Y,X),write(Y),write(","),fail.
+
 b_s(X,Y):-parent(Z,X),parent(Z,Y),man(Z),not(X=Y),!. /*если родные брат/сестра, выводит true, иначе false*/
 b_s_for_all(X,Y):-parent(Z,X),parent(Z,Y),man(Z),not(X=Y).
 b_s(X):-parent(Z,X),parent(Z,Y),man(Z),not(X=Y),write(Y),write(","),fail.
 cousins(X):-parent(Y,X),parent(Z,Y),man(Z),children(W,Z),not(W=Y),allchildren(W),!.
+
 grand_pa(X,Y):-parent(Z,Y),parent(X,Z),man(X),!.
 grand_pas(X):-parent(Y,X),parent(Z,Y),man(Z),write(Z),write(","),fail.
 grand_ma(X,Y):-parent(Z,Y),parent(X,Z),woman(X),!.
 grand_mas(X):-parent(Y,X),parent(Z,Y),woman(Z),write(Z),write(","),fail.
+
+uncle(X,Y):-parent(Z,Y),brother(X,Z).
+uncles(X):-parent(Z,X),brother_for_all(Y,Z),write(Y),write(","),fail.
