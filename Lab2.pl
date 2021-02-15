00148087788e229527af448898b2d16457a11b12
Inessa An <72630506+inessaan@users.noreply.github.com>
2021-02-15 12:31:43 +0300

    Create Lab2.pl
    Предметная область: животные. Всего 30 объектов, 15 вопросов.

---
+++ Lab2.pl
@@ -0,0 +1,568 @@
+predator(lev,1).
+predator(zebra,0).
+predator(gepard,1).
+predator(jiraf,0).
+predator(antilopa,0).
+predator(straus,2).
+predator(stervyatnik,1).
+predator(giena,1).
+predator(shakal,1).
+predator(kobra,1).
+predator(piton,1).
+predator(shimpanze,2).
+predator(mamba,1).
+predator(jako,2).
+predator(marabu,1).
+predator(loshad,0).
+predator(sova,1).
+predator(pantera,1).
+predator(sinica,2).
+predator(korella,2).
+predator(akula,1).
+predator(shuka,1).
+predator(karas,2).
+predator(medusa,1).
+predator(kurica,2).
+predator(svinya,2).
+predator(volk,1).
+predator(sobaka,2).
+predator(fenek,2).
+predator(vorona,2).
+
+
+
+hooves(lev,0).
+hooves(zebra,1).
+hooves(gepard,0).
+hooves(jiraf,1).
+hooves(antilopa,1).
+hooves(straus,0).
+hooves(stervyatnik,0).
+hooves(giena,0).
+hooves(shakal,0).
+hooves(kobra,0).
+hooves(piton,0).
+hooves(shimpanze,0).
+hooves(mamba,0).
+hooves(jako,0).
+hooves(marabu,0).
+hooves(loshad,1).
+hooves(sova,0).
+hooves(pantera,0).
+hooves(sinica,0).
+hooves(korella,0).
+hooves(akula,0).
+hooves(shuka,0).
+hooves(karas,0).
+hooves(medusa,0).
+hooves(kurica,0).
+hooves(svinya,1).
+hooves(volk,0).
+hooves(sobaka,0).
+hooves(fenek,0).
+hooves(vorona,0).
+
+
+
+water(lev,0).
+water(zebra,0).
+water(gepard,0).
+water(jiraf,0).
+water(antilopa,0).
+water(straus,0).
+water(stervyatnik,0).
+water(giena,0).
+water(shakal,0).
+water(kobra,0).
+water(piton,0).
+water(shimpanze,0).
+water(mamba,0).
+water(jako,0).
+water(marabu,0).
+water(loshad,0).
+water(sova,0).
+water(pantera,0).
+water(sinica,0).
+water(korella,0).
+water(akula,1).
+water(shuka,1).
+water(karas,1).
+water(medusa,1).
+water(kurica,0).
+water(svinya,0).
+water(volk,0).
+water(sobaka,0).
+water(fenek,0).
+water(vorona,0).
+
+
+
+cat(lev,1).
+cat(zebra,0).
+cat(gepard,1).
+cat(jiraf,0).
+cat(antilopa,0).
+cat(straus,0).
+cat(stervyatnik,0).
+cat(giena,0).
+cat(shakal,0).
+cat(kobra,0).
+cat(piton,0).
+cat(shimpanze,0).
+cat(mamba,0).
+cat(jako,0).
+cat(marabu,0).
+cat(loshad,0).
+cat(sova,0).
+cat(pantera,1).
+cat(sinica,0).
+cat(korella,0).
+cat(akula,0).
+cat(shuka,0).
+cat(karas,0).
+cat(medusa,0).
+cat(kurica,0).
+cat(svinya,0).
+cat(volk,0).
+cat(sobaka,0).
+cat(fenek,0).
+cat(vorona,0).
+
+
+color(lev,0).
+color(zebra,1).
+color(gepard,2).
+color(jiraf,2).
+color(antilopa,0).
+color(straus,0).
+color(stervyatnik,0).
+color(giena,2).
+color(shakal,0).
+color(kobra,0).
+color(piton,1).
+color(shimpanze,0).
+color(mamba,0).
+color(jako,1).
+color(marabu,0).
+color(loshad,0).
+color(sova,0).
+color(pantera,2).
+color(sinica,0).
+color(korella,0).
+color(akula,0).
+color(shuka,2).
+color(karas,0).
+color(medusa,0).
+color(kurica,0).
+color(svinya,0).
+color(volk,0).
+color(sobaka,0).
+color(fenek,0).
+color(vorona,0).
+
+
+
+
+wings(lev,0).
+wings(zebra,0).
+wings(gepard,0).
+wings(jiraf,0).
+wings(antilopa,0).
+wings(straus,1).
+wings(stervyatnik,1).
+wings(giena,0).
+wings(shakal,0).
+wings(kobra,0).
+wings(piton,0).
+wings(shimpanze,0).
+wings(mamba,0).
+wings(jako,1).
+wings(marabu,1).
+wings(loshad,0).
+wings(sova,1).
+wings(pantera,0).
+wings(sinica,1).
+wings(korella,1).
+wings(akula,0).
+wings(shuka,0).
+wings(karas,0).
+wings(medusa,0).
+wings(kurica,1).
+wings(svinya,0).
+wings(volk,0).
+wings(sobaka,0).
+wings(fenek,0).
+wings(vorona,1).
+
+
+
+size(lev,1).
+size(zebra,1).
+size(gepard,1).
+size(jiraf,1).
+size(antilopa,1).
+size(straus,1).
+size(stervyatnik,0).
+size(giena,1).
+size(shakal,0).
+size(kobra,0).
+size(piton,1).
+size(shimpanze,1).
+size(mamba,1).
+size(jako,1).
+size(marabu,1).
+size(loshad,1).
+size(sova,0).
+size(pantera,1).
+size(sinica,0).
+size(korella,0).
+size(akula,1).
+size(shuka,1).
+size(karas,0).
+size(medusa,0).
+size(kurica,0).
+size(svinya,1).
+size(volk,1).
+size(sobaka,1).
+size(fenek,1).
+size(vorona,1).
+
+
+tame(lev,0).
+tame(zebra,0).
+tame(gepard,0).
+tame(jiraf,0).
+tame(antilopa,0).
+tame(straus,0).
+tame(stervyatnik,0).
+tame(giena,0).
+tame(shakal,0).
+tame(kobra,0).
+tame(piton,0).
+tame(shimpanze,0).
+tame(mamba,0).
+tame(jako,1).
+tame(marabu,0).
+tame(loshad,1).
+tame(sova,1).
+tame(pantera,0).
+tame(sinica,0).
+tame(korella,1).
+tame(akula,0).
+tame(shuka,0).
+tame(karas,0).
+tame(medusa,0).
+tame(kurica,1).
+tame(svinya,1).
+tame(volk,0).
+tame(sobaka,1).
+tame(fenek,0).
+tame(vorona,0).
+
+
+
+dog(lev,0).
+dog(zebra,0).
+dog(gepard,0).
+dog(jiraf,0).
+dog(antilopa,0).
+dog(straus,0).
+dog(stervyatnik,0).
+dog(giena,1).
+dog(shakal,1).
+dog(kobra,0).
+dog(piton,0).
+dog(shimpanze,0).
+dog(mamba,0).
+dog(jako,0).
+dog(marabu,0).
+dog(loshad,0).
+dog(sova,0).
+dog(pantera,0).
+dog(sinica,0).
+dog(korella,0).
+dog(akula,0).
+dog(shuka,0).
+dog(karas,0).
+dog(medusa,0).
+dog(kurica,0).
+dog(svinya,0).
+dog(volk,1).
+dog(sobaka,1).
+dog(fenek,1).
+dog(vorona,0).
+
+
+
+singer(lev,0).
+singer(zebra,0).
+singer(gepard,0).
+singer(jiraf,0).
+singer(antilopa,0).
+singer(straus,0).
+singer(stervyatnik,0).
+singer(giena,0).
+singer(shakal,0).
+singer(kobra,0).
+singer(piton,0).
+singer(shimpanze,0).
+singer(mamba,0).
+singer(jako,1).
+singer(marabu,0).
+singer(loshad,0).
+singer(sova,0).
+singer(pantera,0).
+singer(sinica,1).
+singer(korella,1).
+singer(akula,0).
+singer(shuka,0).
+singer(karas,0).
+singer(medusa,0).
+singer(kurica,0).
+singer(svinya,0).
+singer(volk,0).
+singer(sobaka,0).
+singer(fenek,0).
+singer(vorona,0).
+
+
+toxic(lev,0).
+toxic(zebra,0).
+toxic(gepard,0).
+toxic(jiraf,0).
+toxic(antilopa,0).
+toxic(straus,0).
+toxic(stervyatnik,0).
+toxic(giena,0).
+toxic(shakal,0).
+toxic(kobra,1).
+toxic(piton,0).
+toxic(shimpanze,0).
+toxic(mamba,1).
+toxic(jako,0).
+toxic(marabu,0).
+toxic(loshad,0).
+toxic(sova,0).
+toxic(pantera,0).
+toxic(sinica,0).
+toxic(korella,0).
+toxic(akula,0).
+toxic(shuka,0).
+toxic(karas,0).
+toxic(medusa,1).
+toxic(kurica,0).
+toxic(svinya,0).
+toxic(volk,0).
+toxic(sobaka,0).
+toxic(fenek,0).
+toxic(vorona,0).
+
+
+
+paw(lev,1).
+paw(zebra,0).
+paw(gepard,1).
+paw(jiraf,0).
+paw(antilopa,0).
+paw(straus,1).
+paw(stervyatnik,1).
+paw(giena,1).
+paw(shakal,1).
+paw(kobra,0).
+paw(piton,0).
+paw(shimpanze,1).
+paw(mamba,0).
+paw(jako,1).
+paw(marabu,1).
+paw(loshad,0).
+paw(sova,1).
+paw(pantera,1).
+paw(sinica,1).
+paw(korella,1).
+paw(akula,0).
+paw(shuka,0).
+paw(karas,0).
+paw(medusa,0).
+paw(kurica,1).
+paw(svinya,0).
+paw(volk,1).
+paw(sobaka,1).
+paw(fenek,1).
+paw(vorona,1).
+
+
+river(lev,0).
+river(zebra,0).
+river(gepard,0).
+river(jiraf,0).
+river(antilopa,0).
+river(straus,0).
+river(stervyatnik,0).
+river(giena,0).
+river(shakal,0).
+river(kobra,0).
+river(piton,0).
+river(shimpanze,0).
+river(mamba,0).
+river(jako,0).
+river(marabu,0).
+river(loshad,0).
+river(sova,0).
+river(pantera,0).
+river(sinica,0).
+river(korella,0).
+river(akula,2).
+river(shuka,1).
+river(karas,1).
+river(medusa,2).
+river(kurica,0).
+river(svinya,0).
+river(volk,0).
+river(sobaka,0).
+river(fenek,0).
+river(vorona,0).
+
+
+
+africa(lev,1).
+africa(zebra,1).
+africa(gepard,1).
+africa(jiraf,1).
+africa(antilopa,1).
+africa(straus,1).
+africa(stervyatnik,1).
+africa(giena,1).
+africa(shakal,1).
+africa(kobra,1).
+africa(piton,1).
+africa(shimpanze,1).
+africa(mamba,1).
+africa(jako,1).
+africa(marabu,1).
+africa(loshad,0).
+africa(sova,0).
+africa(pantera,0).
+africa(sinica,0).
+africa(korella,0).
+africa(akula,0).
+africa(shuka,0).
+africa(karas,0).
+africa(medusa,0).
+africa(kurica,0).
+africa(svinya,0).
+africa(volk,0).
+africa(sobaka,0).
+africa(fenek,0).
+africa(vorona,0).
+
+
+fly(lev,0).
+fly(zebra,0).
+fly(gepard,0).
+fly(jiraf,0).
+fly(antilopa,0).
+fly(straus,0).
+fly(stervyatnik,1).
+fly(giena,0).
+fly(shakal,0).
+fly(kobra,0).
+fly(piton,0).
+fly(shimpanze,0).
+fly(mamba,0).
+fly(jako,1).
+fly(marabu,1).
+fly(loshad,0).
+fly(sova,1).
+fly(pantera,0).
+fly(sinica,1).
+fly(korella,1).
+fly(akula,0).
+fly(shuka,0).
+fly(karas,0).
+fly(medusa,0).
+fly(kurica,0).
+fly(svinya,0).
+fly(volk,0).
+fly(sobaka,0).
+fly(fenek,0).
+fly(vorona,1).
+
+
+
+
+
+
+
+question1(X1):-	write("Чем оно питается?"),nl,
+				write("1. Плотоядный"),nl,
+				write("0. Травоядный"),nl,
+        write("2. Всеядный"),nl,
+				read(X1).
+question2(X2):-	write("У него есть копыта?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X2).
+question3(X3):-	write("Оно живет в воде"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X3).
+question4(X4):-	write("Оно из семейства кошачьих?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X4).
+question5(X5):-	write("Есть ли у него особенности расцветки?"),nl,
+				write("1. Полоски"),nl,
+				write("0. Нет"),nl,
+        write("2. Пятнистый"),nl,
+				read(X5).
+question6(X6):-	write("У него есть крылья?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X6).
+question7(X7):-	write("Оно крупное?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X7).
+question8(X8):-	write("Оно одомашнено человеком?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X8).
+question9(X9):-	write("Оно из псовых?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X9).
+question10(X10):-write("Оно умеет петь?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X10).
+question11(X11):-write("Оно ядовитое?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X11).
+question12(X12):-write("У него есть лапы?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X12).
+question13(X13):-write("В какой воде оно живет?"),nl,
+				write("1. Речной"),nl,
+				write("0. Морской"),nl,
+        write("2. Оно не живет в воде"),nl,
+				read(X13).
+question14(X14):-write("Оно живет в Африке?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X14).
+question15(X15):-write("Оно умеет летать?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X15).
+pr:-	question1(X1),question2(X2),question3(X3),question4(X4),
+		  question5(X5),question6(X6),question7(X7),question8(X8),
+      question9(X9),question10(X10),question11(X11),question12(X12),
+      question13(X13),question14(X14),question15(X15),
+
+		predator(X,X1),hooves(X,X2),water(X,X3), cat(X,X4),
+                color(X,X5),wings(X,X6),size(X,X7),tame(X,X8),
+                dog(X,X9),singer(X,X10),toxic(X,X11),paw(X,X12),
+                river(X,X13),africa(X,X14),fly(X,X15),
+		            write(X).
