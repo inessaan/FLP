a9fdc4ec16f097af73af7b98a43ff7454fb353be
Inessa An <72630506+inessaan@users.noreply.github.com>
2021-02-21 16:43:40 +0300

    Задания 1,2

---
+++ Lab2/Lab2.pl
@@ -0,0 +1,599 @@
+predator(lion,1).
+predator(zebra,0).
+predator(cheetah,1).
+predator(giraffe,0).
+predator(antelope_bongo,0).
+predator(ostrich,2).
+predator(vulture,1).
+predator(hyena,1).
+predator(jackal,1).
+predator(cobra,1).
+predator(python,1).
+predator(chimpanzee,2).
+predator(mamba,1).
+predator(jaco,2).
+predator(marabou,1).
+predator(horse,0).
+predator(owl,1).
+predator(panther,1).
+predator(tit,2).
+predator(corella,2).
+predator(shark,1).
+predator(pike,1).
+predator(crucian_carp,2).
+predator(medusa,1).
+predator(chicken,2).
+predator(pig,2).
+predator(wolf,1).
+predator(dog,2).
+predator(fenech,2).
+predator(raven,2).
+
+
+
+hooves(lion,0).
+hooves(zebra,1).
+hooves(cheetah,0).
+hooves(giraffe,1).
+hooves(antelope_bongo,1).
+hooves(ostrich,0).
+hooves(vulture,0).
+hooves(hyena,0).
+hooves(jackal,0).
+hooves(cobra,0).
+hooves(python,0).
+hooves(chimpanzee,0).
+hooves(mamba,0).
+hooves(jaco,0).
+hooves(marabou,0).
+hooves(horse,1).
+hooves(owl,0).
+hooves(panther,0).
+hooves(tomtit,0).
+hooves(corella,0).
+hooves(shark,0).
+hooves(pike,0).
+hooves(crucian_carp,0).
+hooves(medusa,0).
+hooves(chicken,0).
+hooves(pig,1).
+hooves(wolf,0).
+hooves(dog,0).
+hooves(fenech,0).
+hooves(raven,0).
+
+
+
+water(lion,0).
+water(zebra,0).
+water(cheetah,0).
+water(giraffe,0).
+water(antelope_bongo,0).
+water(ostrich,0).
+water(vulture,0).
+water(hyena,0).
+water(jackal,0).
+water(cobra,0).
+water(python,0).
+water(chimpanzee,0).
+water(mamba,0).
+water(jaco,0).
+water(marabou,0).
+water(horse,0).
+water(owl,0).
+water(panther,0).
+water(tomtit,0).
+water(corella,0).
+water(shark,1).
+water(pike,1).
+water(crucian_carp,1).
+water(medusa,1).
+water(chicken,0).
+water(pig,0).
+water(wolf,0).
+water(dog,0).
+water(fenech,0).
+water(raven,0).
+
+
+
+cat(lion,1).
+cat(zebra,0).
+cat(cheetah,1).
+cat(giraffe,0).
+cat(antelope_bongo,0).
+cat(ostrich,0).
+cat(vulture,0).
+cat(hyena,0).
+cat(jackal,0).
+cat(cobra,0).
+cat(python,0).
+cat(chimpanzee,0).
+cat(mamba,0).
+cat(jaco,0).
+cat(marabou,0).
+cat(horse,0).
+cat(owl,0).
+cat(panther,1).
+cat(tomtit,0).
+cat(corella,0).
+cat(shark,0).
+cat(pike,0).
+cat(crucian_carp,0).
+cat(medusa,0).
+cat(chicken,0).
+cat(pig,0).
+cat(wolf,0).
+cat(dog,0).
+cat(fenech,0).
+cat(raven,0).
+
+
+color(lion,0).
+color(zebra,1).
+color(cheetah,2).
+color(giraffe,2).
+color(antelope_bongo,0).
+color(ostrich,0).
+color(vulture,0).
+color(hyena,2).
+color(jackal,0).
+color(cobra,0).
+color(python,1).
+color(chimpanzee,0).
+color(mamba,0).
+color(jaco,1).
+color(marabou,0).
+color(horse,0).
+color(owl,0).
+color(panther,2).
+color(tomtit,0).
+color(corella,0).
+color(shark,0).
+color(pike,2).
+color(crucian_carp,0).
+color(medusa,0).
+color(chicken,0).
+color(pig,0).
+color(wolf,0).
+color(dog,0).
+color(fenech,0).
+color(raven,0).
+
+
+
+
+wings(lion,0).
+wings(zebra,0).
+wings(cheetah,0).
+wings(giraffe,0).
+wings(antelope_bongo,0).
+wings(ostrich,1).
+wings(vulture,1).
+wings(hyena,0).
+wings(jackal,0).
+wings(cobra,0).
+wings(python,0).
+wings(chimpanzee,0).
+wings(mamba,0).
+wings(jaco,1).
+wings(marabou,1).
+wings(horse,0).
+wings(owl,1).
+wings(panther,0).
+wings(tomtit,1).
+wings(corella,1).
+wings(shark,0).
+wings(pike,0).
+wings(crucian_carp,0).
+wings(medusa,0).
+wings(chicken,1).
+wings(pig,0).
+wings(wolf,0).
+wings(dog,0).
+wings(fenech,0).
+wings(raven,1).
+
+
+
+size(lion,1).
+size(zebra,1).
+size(cheetah,1).
+size(giraffe,1).
+size(antelope_bongo,1).
+size(ostrich,1).
+size(vulture,0).
+size(hyena,1).
+size(jackal,0).
+size(cobra,0).
+size(python,1).
+size(chimpanzee,1).
+size(mamba,1).
+size(jaco,1).
+size(marabou,1).
+size(horse,1).
+size(owl,0).
+size(panther,1).
+size(tomtit,0).
+size(corella,0).
+size(shark,1).
+size(pike,1).
+size(crucian_carp,0).
+size(medusa,0).
+size(chicken,0).
+size(pig,1).
+size(wolf,1).
+size(dog,1).
+size(fenech,1).
+size(raven,1).
+
+
+tame(lion,0).
+tame(zebra,0).
+tame(cheetah,0).
+tame(giraffe,0).
+tame(antelope_bongo,0).
+tame(ostrich,0).
+tame(vulture,0).
+tame(hyena,0).
+tame(jackal,0).
+tame(cobra,0).
+tame(python,0).
+tame(chimpanzee,0).
+tame(mamba,0).
+tame(jaco,1).
+tame(marabou,0).
+tame(horse,1).
+tame(owl,1).
+tame(panther,0).
+tame(tomtit,0).
+tame(corella,1).
+tame(shark,0).
+tame(pike,0).
+tame(crucian_carp,0).
+tame(medusa,0).
+tame(chicken,1).
+tame(pig,1).
+tame(wolf,0).
+tame(dog,1).
+tame(fenech,0).
+tame(raven,0).
+
+
+
+dog(lion,0).
+dog(zebra,0).
+dog(cheetah,0).
+dog(giraffe,0).
+dog(antelope_bongo,0).
+dog(ostrich,0).
+dog(vulture,0).
+dog(hyena,1).
+dog(jackal,1).
+dog(cobra,0).
+dog(python,0).
+dog(chimpanzee,0).
+dog(mamba,0).
+dog(jaco,0).
+dog(marabou,0).
+dog(horse,0).
+dog(owl,0).
+dog(panther,0).
+dog(tomtit,0).
+dog(corella,0).
+dog(shark,0).
+dog(pike,0).
+dog(crucian_carp,0).
+dog(medusa,0).
+dog(chicken,0).
+dog(pig,0).
+dog(wolf,1).
+dog(dog,1).
+dog(fenech,1).
+dog(raven,0).
+
+
+
+singer(lion,0).
+singer(zebra,0).
+singer(cheetah,0).
+singer(giraffe,0).
+singer(antelope_bongo,0).
+singer(ostrich,0).
+singer(vulture,0).
+singer(hyena,0).
+singer(jackal,0).
+singer(cobra,0).
+singer(python,0).
+singer(chimpanzee,0).
+singer(mamba,0).
+singer(jaco,1).
+singer(marabou,0).
+singer(horse,0).
+singer(owl,0).
+singer(panther,0).
+singer(tomtit,1).
+singer(corella,1).
+singer(shark,0).
+singer(pike,0).
+singer(crucian_carp,0).
+singer(medusa,0).
+singer(chicken,0).
+singer(pig,0).
+singer(wolf,0).
+singer(dog,0).
+singer(fenech,0).
+singer(raven,0).
+
+
+toxic(lion,0).
+toxic(zebra,0).
+toxic(cheetah,0).
+toxic(giraffe,0).
+toxic(antelope_bongo,0).
+toxic(ostrich,0).
+toxic(vulture,0).
+toxic(hyena,0).
+toxic(jackal,0).
+toxic(cobra,1).
+toxic(python,0).
+toxic(chimpanzee,0).
+toxic(mamba,1).
+toxic(jaco,0).
+toxic(marabou,0).
+toxic(horse,0).
+toxic(owl,0).
+toxic(panther,0).
+toxic(tomtit,0).
+toxic(corella,0).
+toxic(shark,0).
+toxic(pike,0).
+toxic(crucian_carp,0).
+toxic(medusa,1).
+toxic(chicken,0).
+toxic(pig,0).
+toxic(wolf,0).
+toxic(dog,0).
+toxic(fenech,0).
+toxic(raven,0).
+
+
+
+paw(lion,1).
+paw(zebra,0).
+paw(cheetah,1).
+paw(giraffe,0).
+paw(antelope_bongo,0).
+paw(ostrich,1).
+paw(vulture,1).
+paw(hyena,1).
+paw(jackal,1).
+paw(cobra,0).
+paw(python,0).
+paw(chimpanzee,1).
+paw(mamba,0).
+paw(jaco,1).
+paw(marabou,1).
+paw(horse,0).
+paw(owl,1).
+paw(panther,1).
+paw(tomtit,1).
+paw(corella,1).
+paw(shark,0).
+paw(pike,0).
+paw(crucian_carp,0).
+paw(medusa,0).
+paw(chicken,1).
+paw(pig,0).
+paw(wolf,1).
+paw(dog,1).
+paw(fenech,1).
+paw(raven,1).
+
+
+river(lion,0).
+river(zebra,0).
+river(cheetah,0).
+river(giraffe,0).
+river(antelope_bongo,0).
+river(ostrich,0).
+river(vulture,0).
+river(hyena,0).
+river(jackal,0).
+river(cobra,0).
+river(python,0).
+river(chimpanzee,0).
+river(mamba,0).
+river(jaco,0).
+river(marabou,0).
+river(horse,0).
+river(owl,0).
+river(panther,0).
+river(tomtit,0).
+river(corella,0).
+river(shark,2).
+river(pike,1).
+river(crucian_carp,1).
+river(medusa,2).
+river(chicken,0).
+river(pig,0).
+river(wolf,0).
+river(dog,0).
+river(fenech,0).
+river(raven,0).
+
+
+
+africa(lion,1).
+africa(zebra,1).
+africa(cheetah,1).
+africa(giraffe,1).
+africa(antelope_bongo,1).
+africa(ostrich,1).
+africa(vulture,1).
+africa(hyena,1).
+africa(jackal,1).
+africa(cobra,1).
+africa(python,1).
+africa(chimpanzee,1).
+africa(mamba,1).
+africa(jaco,1).
+africa(marabou,1).
+africa(horse,0).
+africa(owl,0).
+africa(panther,0).
+africa(tomtit,0).
+africa(corella,0).
+africa(shark,0).
+africa(pike,0).
+africa(crucian_carp,0).
+africa(medusa,0).
+africa(chicken,0).
+africa(pig,0).
+africa(wolf,0).
+africa(dog,0).
+africa(fenech,0).
+africa(raven,0).
+
+
+fly(lion,0).
+fly(zebra,0).
+fly(cheetah,0).
+fly(giraffe,0).
+fly(antelope_bongo,0).
+fly(ostrich,0).
+fly(vulture,1).
+fly(hyena,0).
+fly(jackal,0).
+fly(cobra,0).
+fly(python,0).
+fly(chimpanzee,0).
+fly(mamba,0).
+fly(jaco,1).
+fly(marabou,1).
+fly(horse,0).
+fly(owl,1).
+fly(panther,0).
+fly(tomtit,1).
+fly(corella,1).
+fly(shark,0).
+fly(pike,0).
+fly(crucian_carp,0).
+fly(medusa,0).
+fly(chicken,0).
+fly(pig,0).
+fly(wolf,0).
+fly(dog,0).
+fly(fenech,0).
+fly(raven,1).
+
+horns(lion,0).
+horns(zebra,0).
+horns(cheetah,0).
+horns(giraffe,1).
+horns(antelope_bongo,1).
+horns(ostrich,0).
+horns(vulture,0).
+horns(hyena,0).
+horns(jackal,0).
+horns(cobra,0).
+horns(python,0).
+horns(chimpanzee,0).
+horns(mamba,0).
+horns(jaco,0).
+horns(marabou,0).
+horns(horse,0).
+horns(owl,0).
+horns(panther,0).
+horns(tomtit,0).
+horns(corella,0).
+horns(shark,0).
+horns(pike,0).
+horns(crucian_carp,0).
+horns(medusa,0).
+horns(chicken,0).
+horns(pig,1).
+horns(wolf,0).
+horns(dog,0).
+horns(fenech,0).
+horns(raven,0).
+
+
+question1(X1):-	write("1. What does it eat?"),nl,
+				write("1. carnivorous"),nl,
+				write("0. herbivore"),nl,
+                                write("2. omnivore"),nl,
+				read(X1).
+question2(X2):-	write("2. Does it have hooves?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X2).
+question3(X3):-	write("3. Does it live in water?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X3).
+question4(X4):-	write("4. Is it from the cat family?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X4).
+question5(X5):-	write("5. Does it have any color features?"),nl,
+				write("1. stripes"),nl,
+				write("0. not always"),nl,
+                                write("2. spotted"),nl,
+				read(X5).
+question6(X6):-	write("6. Does it have wings?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X6).
+question7(X7):-	write("7. Is it big?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X7).
+question8(X8):-	write("8. Is it domesticated by man?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X8).
+question9(X9):-	write("9. Is it from the canine family?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X9).
+question10(X10):-write("10. Оно умеет петь?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X10).
+question11(X11):-write("11. Is it poisonous?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X11).
+question12(X12):-write("12. Does it have paws?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X12).
+question13(X13):-write("13. What kind of water does it live in?"),nl,
+				write("1. river"),nl,
+				write("0. marine"),nl,
+                                write("2. it doesn't live in water"),nl,
+				read(X13).
+question14(X14):-write("14. Does it live in Africa?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X14).
+question15(X15):-write("15. Can it fly?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X15).
+question16(X16):-write("16. Does it have horns?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X16).
+
+pr:-	question1(X1),question2(X2),question3(X3),question4(X4),
+		question5(X5),question6(X6),question7(X7),question8(X8),
+                question9(X9),question10(X10),question11(X11),question12(X12),
+                question13(X13),question14(X14),question15(X15),question16(X16),
+
+		predator(X,X1),hooves(X,X2),water(X,X3), cat(X,X4),
+                color(X,X5),wings(X,X6),size(X,X7),tame(X,X8),
+                dog(X,X9),singer(X,X10),toxic(X,X11),paw(X,X12),
+                river(X,X13),africa(X,X14),fly(X,X15),horns(X,X16),
+		write(X).
