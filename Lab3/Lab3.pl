8c3cd90de915f0ea2fc7dbb692394f617bf7ed32
Inessa An <72630506+inessaan@users.noreply.github.com>
2021-02-23 21:53:38 +0300

    Задание 5
    Предикат fib(N,X), где X – число Фибоначчи с
    номером N, причем 1 и 2 элемент равны 1 с помощью рекурсии вверх.

--- Lab3/Lab3.pl
+++ Lab3/Lab3.pl
@@ -11,3 +11,7 @@ fact(N,X):-N1 is N-1, fact(N1,X1), X is N*X1.
 fact_down(1,CurX,CurX):-!.
 fact_down(N,CurX,X):-N1 is N-1, CurX1 is CurX*N, fact_down(N1,CurX1,X).
 factorial_down(N,X):-fact_down(N,1,X).
+
+fib(1, 1) :- !.
+fib(2, 1) :- !.
+fib(N, X) :- N1 is N - 1, N2 is N - 2, fib(N1, X1), fib(N2, X2), X is X1 + X2.
