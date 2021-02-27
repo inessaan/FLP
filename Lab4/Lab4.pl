140c3fce651f1579555e8a669f94865e4dbcdafc
Inessa An <72630506+inessaan@users.noreply.github.com>
2021-02-27 17:57:49 +0300

    Задание 2

--- Lab4/Lab4.pl
+++ Lab4/Lab4.pl
@@ -4,3 +4,12 @@ writeList([H|L]):-write(H),writeList(L).
 readList(N,L):-rList(N,0,[],L).
 rList(N,N,L,L):-!.
 rList(N,I,CurL,L):-I1 is I+1, read(X), append(CurL, [X],CurL1), rList(N,I1,CurL1,L).
+
+sumListDown(List,Sum):-sListD(List,0,Sum).
+sListD([],Sum,Sum):-!.
+sListD([H|L],CurSum,Sum):-CurSum1 is CurSum + H, sListD(L,CurSum1,Sum).
+
+readSumWriteList(N):-
+    readList(N,L),
+    sumListDown(L,Sum),
+    nl, write("Sum = "),write(Sum),nl,fail.
