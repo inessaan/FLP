2f6e6292485f0c9b7e76522c7b8af58725124916
Inessa An <72630506+inessaan@users.noreply.github.com>
2021-02-28 18:03:12 +0300

    Задание 8

--- Lab4/Lab4.pl
+++ Lab4/Lab4.pl
@@ -42,3 +42,6 @@ minListD([H|T],Min):-minListD(T,H,Min).
 minListD([],Min,Min):-!.
 minListD([H|T],Cur,Min):-H<Cur,minListD(T,H,Min),!.
 minListD([_|T],Cur,Min):-minListD(T,Cur,Min).
+
+/*Задание 8*/
+readLMin:-write("N = "),read(N), nl,readList(N,List),minListUp(List,Min),write("min = "),write(Min).
